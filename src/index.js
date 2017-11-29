dcef7546f557a3fd17fcd59541d2f43d8a304ab0
VuongQuangTuyen <TuyenGa@techmasters-iMac-8.local>
2017-11-29 17:47:06 +0700

    push components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React, { Component } from "react";
 
-import App from './components/app';
-import reducers from './reducers';
+import ReactDOM from "react-dom";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {
+    return <h1>Hi ! </h1> ;
+};
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <App /> , document.querySelector('.container')
+);
